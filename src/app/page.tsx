ac29835cc740068f0bbf27948339f645751671f1
Robass Atif <mohammadrobass@gmail.com>
2024-07-07 09:36:36 +0500

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,27 @@
-import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
- import Navbar from  '../Navbar' 
-import { redirect } from 'next/navigation'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import Navbar from '../Navbar';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  const {isAuthenticated,getUser}=getKindeServerSession();
+  const { isAuthenticated, getUser } = getKindeServerSession();
 
-  
-
-  if(!(await isAuthenticated())){
-    return redirect(
-      "/api/auth/login?post_login_redirect_url=https://techtutors.vercel.app/");
+  // Check if the user is authenticated
+  if (!(await isAuthenticated())) {
+    return redirect("/api/auth/verify-email?post_verify_email_redirect_url=http://localhost:3000/");
   }
 
-const user=await getUser();
+  const user = await getUser();
 
-if (!user) {
-  return redirect(
-    "/api/auth/verify-email?post_verify_email_redirect_url=https://techtutors.vercel.app/");
-}
-
-
-
-
-    return (
+  // Check if the user is verified
+  if (!user) {
+    return redirect("//api/auth/verify-email?post_verify_email_redirect_url=http://localhost:3000/");
+  }
+// /api/auth/verify-email?post_verify_email_redirect_url=https://techtutors.vercel.app/
+  // Return the component if user is authenticated and verified
+  return (
     <>
-    <Navbar />
-    hi {user.email}
+      
+      hi {user.email}
     </>
   );
 }
