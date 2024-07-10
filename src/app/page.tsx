15536cd1f5ffaaaf1b292e48fe7b06dddda1cdac
Robass Atif <mohammadrobass@gmail.com>
2024-07-10 19:04:34 +0500

    asdaasdadsad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default async function Home() {
   const { isAuthenticated, getUser } = getKindeServerSession();
 
   if (!(await isAuthenticated())) {
-    return redirect("/api/auth/login?post_login_redirect_url=https://techtutor.vercel.app/");
+    return redirect("/api/auth/login?post_login_redirect_url=https://techtutors.vercel.app/");
   }
 
   const user = await getUser();
 
   if (!user) {
-    return redirect("/api/auth/verify-email?post_verify_email_redirect_url=https://techtutor.vercel.app/");
+    return redirect("/api/auth/verify-email?post_verify_email_redirect_url=https://techtutors.vercel.app/");
   }
 
   return (
